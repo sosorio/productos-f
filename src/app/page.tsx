b60801ccdad227f82215930075a051783923b6e9
Santiago Osorio <sosorio1973@gmail.com>
2024-08-30 18:09:49 -0500

    2

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-Texto ejemplo aca
+Texto ejemplo aca..segundo
     </main>
   );
 }
