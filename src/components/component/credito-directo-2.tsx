8074e4d50200f046519ed8760a2090760dd82555
Santiago Osorio <sosorio1973@gmail.com>
2024-08-31 16:28:15 -0500

    inicio paginas lineas 3

--- src/components/component/credito-directo-2.tsx
+++ src/components/component/credito-directo-2.tsx
@@ -219,24 +219,12 @@ export function CreditoDirecto2() {
  Otorgamos junto a entidades de derecho internacional, créditos directo con o sin tasa compensada en los que podamos convenir una participación en beneficio de las entidades territoriales para la financiación de proyectos de inversión en los sectores y subsectores elegibles de Findeter.
               </p>
             </div>
-            <div className="mx-auto w-full max-w-sm space-y-2">
-              <form className="flex gap-2">
-                <Input type="email" placeholder="Ingrese su correo electrónico" className="max-w-lg flex-1" />
-                <Button type="submit">Empezar</Button>
-              </form>
-              <p className="text-xs text-muted-foreground">
-                Al registrarse, acepta nuestros{" "}
-                <Link href="#" className="underline underline-offset-2" prefetch={false}>
-                  Términos de Servicio
-                </Link>
-                .
-              </p>
-            </div>
+
           </div>
         </section>
       </main>
       <footer className="flex flex-col gap-2 sm:flex-row py-6 w-full shrink-0 items-center px-4 md:px-6 border-t">
-        <p className="text-xs text-muted-foreground">&copy; 2024 Acme Inc. Todos los derechos reservados.</p>
+        <p className="text-xs text-muted-foreground">&copy; 2024 Acme Inc. PAGINA DE PRUEBA.</p>
         <nav className="sm:ml-auto flex gap-4 sm:gap-6">
           <Link href="#" className="text-xs hover:underline underline-offset-4" prefetch={false}>
             Términos de Servicio
