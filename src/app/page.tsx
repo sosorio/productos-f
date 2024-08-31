9c52e9028a149fc1e959613edf8e2a48083a0e6b
Santiago Osorio <sosorio1973@gmail.com>
2024-08-31 11:15:15 -0500

    Con el componente credito directo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,10 @@
 import Image from "next/image";
+import  { CreditoDirecto2 } from "@/components/component/credito-directo-2";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-Texto ejemplo aca..segundo
+<CreditoDirecto2/>
     </main>
   );
 }
