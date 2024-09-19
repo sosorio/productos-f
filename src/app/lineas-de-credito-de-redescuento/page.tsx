8d0717a4750901c409d30b240151c0ea09aafb13
Santiago Osorio <sosorio1973@gmail.com>
2024-09-19 14:15:55 -0500

    con las paginas de redescuento

---
+++ src/app/lineas-de-credito-de-redescuento/page.tsx
@@ -0,0 +1,12 @@
+
+
+import { Redescuento } from "@/components/component/redescuento";
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-4">
+      <p>Esta es una pagina de desarrollo, para ajustes y diseño.</p>
+<Redescuento/>
+    </main>
+  );
+}
---
+++ src/app/lineas-de-credito-de-redescuento/programa-reactiva-colombia-findeter/page.tsx
@@ -0,0 +1,13 @@
+
+
+import { RedescuentoReactivaColombia } from "@/components/component/redescuento-reactiva-colombia";
+
+
+export default function Home() {
+  return (
+    <main className="flex flex-col  justify-between p-4">
+      <p>Esta es una pagina de desarrollo, para ajustes y diseño.</p>
+<RedescuentoReactivaColombia/>
+    </main>
+  );
+}
